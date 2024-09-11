87769b5f9e1e2791170793c4ad5fbda3e3365061
Gustavo Gettar <gus.gettar@gmail.com>
2024-09-11 13:58:14 -0300

    ejercicio 2

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import HolaMundo from "./HolaMundo"
+
+function App() {
+
+
+  return (
+  <HolaMundo friend={'hello my friend'}></HolaMundo>
+  )
+}
+
+export default App
---
+++ src/HolaMundo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const HolaMundo = ({friend}) => {
+    return (
+        <div>
+           {friend}
+        </div>
+    );
+};
+
+export default HolaMundo;
